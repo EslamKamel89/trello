d2cb8317b014489e1c35943807675503b0c74c50
EslamKamel89 <eslam_kamel_89@hotmail.com>
2024-09-14 11:28:00 +0300

    creating board column ui design

--- resources/views/livewire/board-show.blade.php
+++ resources/views/livewire/board-show.blade.php
@@ -1,7 +1,20 @@
-<div>
+<div class="h-full">
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __( $board?->title ?? 'Board Not Found' ) }}
+        <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
+            {{ __( $board?->title ?? "Board Not Found" ) }}
         </h2>
     </x-slot>
+    <!--//! Main Container -->
+    <div class="w-full h-max p-6 overflow-x-scroll">
+        <!--//? Flex Container -->
+        <div class="flex w-max space-x-6 bg-green-600 h-[calc(theme('height.screen')-240px)]">
+            @foreach ( range( 1, 10 ) as $column )
+				<!--//! Column -->
+				<div class="w-[260px] bg-blue-500">Column</div>
+				<!--// Column -->
+			@endforeach
+        </div>
+        <!-- Flex Container -->
+    </div>
+    <!-- Main Container -->
 </div>
